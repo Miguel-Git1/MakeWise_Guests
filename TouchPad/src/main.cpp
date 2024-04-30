83c7e1399c9495e396dfe65309b2f597a87f41ad
Delcio Silva <delciobvsilva@gmail.com>
2024-04-30 17:58:07 +0100

    Sensor Touchpad

---
+++ TouchPad/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000); // give me time to bring up serial monitor
+  Serial.println("ESP32 Touch Test");
+}
+
+void loop()
+{
+  Serial.println(touchRead(4)); // get value of Touch 0 pin = GPIO 4
+  delay(1000);
+}
