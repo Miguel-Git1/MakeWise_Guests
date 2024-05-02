474e08d9b4fccb9c76d67d28909f780338f6e533
Delcio Silva <delciobvsilva@gmail.com>
2024-05-02 12:13:58 +0100

    Touchpad com classes e led 😎

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int led = 2;
 
 void setup()
 {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
 }
 
 // put function definitions here:
--- TouchPad/src/main.cpp
+++ TouchPad/src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
+#include "button.h"
+#include "led.h"
 
+Led bruno(2);
+Button mathew(4, 20, 50);
 void setup()
 {
   Serial.begin(9600);
   delay(1000); // give me time to bring up serial monitor
-  Serial.println("ESP32 Touch Test");
 }
 
 void loop()
 {
-  Serial.println(touchRead(4)); // get value of Touch 0 pin = GPIO 4
-  delay(1000);
+  mathew.update();
+  if (mathew.state())
+  {
+    bruno.switchState();
+  }
+  delay(500);
 }
