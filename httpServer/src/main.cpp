9a09bf4e0932adacb540a4909ce190a2148e5f34
Suiby <miguelmews123vv@gmail.com>
2024-05-02 12:39:45 +0100

    Servidor Web 🔥🔥🔥🔥🔥🔥

---
+++ httpServer/src/main.cpp
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+const char* ssid = "";
+const char* password = "";
+
+WiFiServer server(80);
+String header;
+
+// Led State
+String outputState = "off";
+
+// Led pin
+const int output = 2;
+
+
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("");
+  Serial.print("Wifi connected.");
+  Serial.print("I address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+
+  if (client)
+  {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected())
+    {
+      char c = client.read();
+      Serial.write(c);
+      header += c;
+      if (c == '\n')
+      {
+        if (currentLine.length() == 0)
+        {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+        
+
+
+        if (header.indexOf("GET /26/on") >= 0) 
+        {
+          Serial.println("GPIO 26 on");
+          outputState = "on";
+          digitalWrite(output, HIGH);
+        } 
+        else if (header.indexOf("GET /26/off") >= 0) 
+        {
+          Serial.println("GPIO 26 off");
+          outputState = "off";
+          digitalWrite(output, LOW);
+        }
+
+        client.println("<!DOCTYPE html><html>");
+        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+        client.println("<link rel=\"icon\" href=\"data:,\">");
+        // CSS to style the on/off buttons 
+        // Feel free to change the background-color and font-size attributes to fit your preferences
+        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+        client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+        client.println(".button2 {background-color: #555555;}</style></head>");
+            
+        // Web Page Heading
+        client.println("<body><h1>ESP32 Web Server</h1>");
+
+        client.println("<p> GPIO - State " + outputState + "</p>");
+        if (outputState == "off")
+        {
+          client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+        }
+        else 
+        {
+          client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+        }
+        client.println("</body></html>");
+
+        client.println();
+        break;
+      }
+      else
+      {
+        currentLine = "";
+      }
+
+    }
+    else if (c != '\r')
+    {
+      currentLine += c;
+    }
+  }
+  header = "";
+  client.stop();
+  Serial.println("Client disconnected.");
+  Serial.println("");
+  }
+}
